e79b3332bb2f2424d62da07217135883d83cfc01
Arin Yadav <arinyadav98@gmail.com>
2025-03-06 20:04:04 +0530

    update code

--- OOPs in Java/Encapsulation.java
+++ OOPs in Java/Encapsulation.java
@@ -2,7 +2,9 @@ import bank.*;
 
 public class Encapsulation {
     public static void main(String[] args) {
-        System.out.println("Hii");
-        Bank cd = new Bank();        
+        System.out.println("Bank Customer Details");
+        Bank bank = new Bank(); 
+        bank.setCustomerDetails("Ankit Yadav", 1234567890L, "ankit123@gmail.com", 1234567890L, 1000000.0);
+        System.out.println(bank.getName());      
     }
 }
--- OOPs in Java/bank/Bank.java
+++ OOPs in Java/bank/Bank.java
@@ -1,12 +1,26 @@
 package bank;
-class Account{
-    public String name;
-}
+
 public class Bank {
-    public static void main(String[] args) {
-        System.out.println("Hi");
+    
+   
+    private String name;
+    private Long accountNumber;
+    private String email;
+    private Long phoneNumber;
+    private double balance;
+    
+    public void setCustomerDetails(String name, Long accountNumber, String email, Long phoneNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.balance = balance;    
     }
 
-    public class Account {
+    
+
+    public String getName() {
+       return "Name: " + name +"\n" + "Account Number: " + accountNumber +"\n" +"Email: " + email +"\n" + "Phone Number: " + phoneNumber +"\n" + "Balance: " + balance;
     }
+
 }
